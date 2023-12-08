640ca4d5bd3b78516dc5d9c64b848aa5aa1e6c47
KimDohyoun79 <dogdo5726@gmail.com>
2023-12-08 17:25:03 +0900

    [feat] About Function
    - 함수 선언식 / 함수 표현식 / Function 생성자 함수

---
+++ javascript/01_basic/11_function/js/common.js
@@ -0,0 +1,9 @@
+/**
+ * 이 함수는 제품 가격에 대한 부가세를 반환하는 함수입니다.
+ * @param {*} productAmount 제품가격을 숫자형 전달 
+ * @returns 부가세가 숫자형
+ */
+
+function taxAmount(productAmount){
+  return productAmount * 0.1;
+}
